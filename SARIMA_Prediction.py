e1488687d46698d9ae0c06facd22326b0f13fd87
darshans0200 <darshans020@gmail.com>
2019-03-11 22:20:17 +0530

    Added model details
    Added model, prediction and saved pickle file

---
+++ SARIMA_Prediction.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  6 09:36:29 2019
+
+@author: Darshan
+"""
+import pickle
+import sys
+import pandas as pd
+import warnings
+#import itertools
+#import numpy as np
+#import matplotlib.pyplot as plt
+warnings.filterwarnings("ignore")
+#plt.style.use('fivethirtyeight')
+import pandas as pd
+import statsmodels.api as sm
+#import matplotlib
+import datetime
+import mysql.connector
+
+'''
+matplotlib.rcParams['axes.labelsize'] = 14
+matplotlib.rcParams['xtick.labelsize'] = 12
+matplotlib.rcParams['ytick.labelsize'] = 12
+matplotlib.rcParams['text.color'] = 'k'
+'''
+Product=sys.argv[1]
+start_date=sys.argv[2]
+end_date=sys.argv[3]
+version ='v1'
+print ('Number of arguments:', len(sys.argv), 'arguments.')
+print ('Argument List:', str(sys.argv))
+
+
+Product=Product.upper()
+modelfile=Product+'-model_SARIMA.pkl'
+SARIMA_model_pkl = open(modelfile, 'rb')
+pickle_model = pickle.load(SARIMA_model_pkl)
+print ("Loaded SARIMA model :: ", pickle_model)
+Forecast = pickle_model.predict(start=start_date, end=end_date)
+Forecast=Forecast.to_frame(name=None)
+Forecast=Forecast.rename(columns = {0:'Quantity_predicated'})
+index1 = pd.date_range(start=start_date, end=end_date)
+Forecast['Stime']=index1
+print(Forecast.head())
+
+#prod 
+mydb=mysql.connector.connect(host="43.224.157.214", user="siddas", passwd="siddas@123", database="diztro_reports")
+#UP's
+#mydb=mysql.connector.connect(host="localhost", user="siddas", passwd="siddas@123", database="diztro_reports")
+mycursor = mydb.cursor()
+#sql = "INSERT INTO dldump (storeid, fname,ProcessedPath,status,fridgeid) VALUES (%s, %s,%s, %s,%s)"
+
+dict=[] #date, quantity, version, product
+for index,row in Forecast.iterrows():
+    dict.append((index,row['Quantity_predicated'],version,Product))
+    print (index,row['Quantity_predicated'])
+    
+sql1="INSERT INTO ml_daily_estimation (Date, quantity,version,product) VALUES (%s, %s,%s,%s)"
+mycursor.executemany(sql1, dict)
+mydb.commit()
+
+Forecast=Forecast.resample('M', on='Stime').sum()
+print('monthly estimations')
+print(Forecast)
+year,month,day=start_date.split('-')
+yrmonth=month+'/'+year
+dict2=[] #date, quantity, version, product
+Forecast_month=Forecast.iloc[0]
+#ind=row_iterator.next()
+print('first row estmiat',Forecast_month)
+'''
+for index,row in Forecast.iterrows():
+    dict2.append((index,row['Quantity_predicated'],version,Product))
+    print (index,row['Quantity_predicated'])
+''' 
+#print(mydb) 
+
+oldstart=(datetime.datetime.strptime(start_date,"%Y-%m-%d").date()) -datetime.timedelta(days=183)
+#print(oldstart)
+oldend=(datetime.datetime.strptime(start_date,"%Y-%m-%d").date()) -datetime.timedelta(days=1)
+#print(oldend)
+Forecast_old = pickle_model.predict(start=oldstart, end=oldend)
+Forecast_old=Forecast_old.to_frame(name=None)
+Forecast_old=Forecast_old.rename(columns = {0:'Quantity_predicated'})
+index = pd.date_range(start=oldstart, end=oldend)
+Forecast_old['Stime']=index
+
+#Forecast_old.set_index('Stime', inplace=True)
+
+Forecast_old=Forecast_old.resample('M', on='Stime').sum()
+print(Forecast_old)
+
+#Forecast_old = Forecast_old['Quantity_predicated'].resample('M').sum()
+
+average=Forecast_old['Quantity_predicated'].mean()
+
+print(average)
+target=(Forecast_month-average)/average
+print(target)
+
+#use it whenever required
+#target.reset_index(inplace=True) # Resets the index, makes factor a column
+#target.drop('Stime',axis=1,inplace=True)
+
+
+#target=target.drop(['Stime'],axis=1)
+percent=target['Quantity_predicated']
+print(type(percent))
+print(percent)
+month_tgt=Forecast_month['Quantity_predicated']
+val2 = (yrmonth, Product,'All',percent,month_tgt)
+print(val2)
+sql2 = "INSERT INTO ml_monthly_estimation (YearMonth,Product,item_number_id,target_percent,target_quantity) VALUES (%s,%s,%s,%s,%s)"
+mycursor.execute(sql2, val2)
+mydb.commit()
+
+mycursor.close()
+mydb.close()
+
+SARIMA_model_pkl.close()
---
+++ SARIMA_Product.py
@@ -0,0 +1,222 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # SARIMA
+import warnings
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import statsmodels.api as sm
+import matplotlib
+import pickle
+
+import statsmodels.api as sm  
+from statsmodels.tsa.stattools import acf  
+from statsmodels.tsa.stattools import pacf
+from statsmodels.tsa.seasonal import seasonal_decompose
+
+def timeseries_train_test_split(paneer, test_size):
+    """
+        Perform train-test split with respect to time series structure
+    """
+    
+    # get the index after which test set starts
+    test_index = int(len(paneer)*(1-test_size))
+    
+    X_train = paneer.iloc[:test_index]
+    X_test = paneer.iloc[test_index:]
+
+    
+    return X_train, X_test
+
+warnings.filterwarnings("ignore")
+plt.style.use('fivethirtyeight')
+
+matplotlib.rcParams['axes.labelsize'] = 14
+matplotlib.rcParams['xtick.labelsize'] = 12
+matplotlib.rcParams['ytick.labelsize'] = 12
+matplotlib.rcParams['text.color'] = 'k'
+
+
+df = pd.read_csv('C:\\Users\\Darshan\\Downloads\\Misty\\revised\\name1.csv', sep=',')
+df['Stime'] = pd.to_datetime(df['Stime'],format="%d-%m-%Y")
+
+
+df=df.drop(['SALESEXECUTIVEID','ASMID','ZONALHEADID','SALESHEADID'],axis=1)
+
+cols = list(pd.read_csv('C:\\Users\\Darshan\\Downloads\\Misty\\revised\\product1.csv', nrows =1))
+print(cols)
+
+prod=pd.read_csv('C:\\Users\\Darshan\\Downloads\\Misty\\revised\\product1.csv', sep=',',usecols =[i for i in cols if i != ('description')])
+prod=prod.drop(['Createdt', 'CreateID','productId', 'productCategory','Updatedt','activeProduct','minimumRate','maximumRate','photo','updateID','HSNCode'], axis=1)
+
+m=pd.merge(df, prod, on='Item_number_ID')
+
+products=m.Product.unique()
+#products=m.loc[(['Product']!='MILK')]
+print (products)
+for item in products:
+    print (item)
+    if item=='MILK':
+        continue
+#paneer=m.loc[(m['Product'] == 'PANEER')]
+    paneer=m.loc[(m['Product']==item)]
+    cols = ['Id', 'Year', 'Month', 'Day', 'Item_number_ID',
+            'Customer_Account_ID', 'regionID', 'Amount', 'productName',
+            'Product', 'Sub_Product', 'Item_name', 'weight', 'weightUnit',
+            'shelfLife', 'purchaseRate', 'retailRate', 'GSTPercentage']
+    paneer.drop(cols, axis=1, inplace=True)
+    paneer=paneer.resample('D', on='Stime').sum()
+
+    X_train, X_test = timeseries_train_test_split(paneer, test_size=0.3)
+    p = d = q = range(0, 2)
+    pdq = list(itertools.product(p, d, q))
+    seasonal_pdq = [(x[0], x[1], x[2], 12) for x in list(itertools.product(p, d, q))]
+    for param in pdq:
+        for param_seasonal in seasonal_pdq:
+            try:
+                mod = sm.tsa.statespace.SARIMAX(X_train,
+                                                order=param,
+                                                seasonal_order=param_seasonal,
+                                                enforce_stationarity=False,
+                                                enforce_invertibility=False)
+    
+                results = mod.fit()
+    
+                print('ARIMA{}x{}12 - AIC:{}'.format(param, param_seasonal, results.aic))
+            except:
+                continue
+    
+
+    mod = sm.tsa.statespace.SARIMAX(X_train,
+                                    order=(1, 1, 1),
+                                    seasonal_order=(0, 1, 1, 12),
+                                    enforce_stationarity=False,
+                                    enforce_invertibility=False)
+
+    results = mod.fit()
+    
+  
+    pickle.dump(results, open(item+"-model_SARIMA.pkl", "wb"))
+
+    #print(results.summary().tables[1])
+
+    #start_index = '2018-11-01'
+    #end_index = '2019-01-31'
+    #forecast_test = results.predict(start=start_index, end=end_index)
+
+    #forecast_test.head()
+
+    #forecast_test=forecast_test.to_frame(name=None)
+
+    #forecast_test=forecast_test.rename(columns = {0:'Quantity_predicated'})
+
+    #index = pd.date_range(start="2018-11-01", end="2019-01-31")
+
+    #forecast_test['Stime']=index
+
+    #forecast_test.set_index('Stime', inplace=True)
+
+'''
+forecast_test_error = X_test['Quantity']-forecast_test['Quantity_predicated']
+
+mean_forecast_test_error = np.mean(forecast_test_error)
+print(mean_forecast_test_error)
+
+mean_absolute_test_error = np.mean( np.abs(forecast_test_error) )
+print(mean_absolute_test_error)
+
+mean_squared_test_error = np.mean(forecast_test_error*forecast_test_error)
+print(mean_squared_test_error)
+
+rmse_test = np.sqrt(mean_squared_test_error)
+print(rmse_test)
+
+def mean_absolute_percentage_test_error(y_true, y_pred): 
+    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
+
+MAPE= mean_absolute_percentage_test_error(X_test['Quantity'],forecast_test['Quantity_predicated'])
+MAPE
+X_test.shape,forecast_test.shape
+
+pjme_test=pd.merge(X_test,forecast_test, left_index=True, right_index=True)
+
+MAPE= mean_absolute_percentage_train_error(pjme_test['Quantity'],pjme_test['Quantity_predicated'])
+MAPE
+
+pjme_test.plot(figsize=(15, 6))
+plt.show()
+
+#pred_uc = results.get_forecast(steps=92)
+#pred_ci = pred_uc.conf_int()
+
+start_index = '2018-04-01'
+end_index = '2018-10-31'
+forecast_train = results.predict(start=start_index, end=end_index)
+
+forecast_train=forecast_train.to_frame(name=None)
+
+forecast_train=forecast_train.rename(columns = {0:'Quantity_predicated'})
+forecast_train.head()
+forecast_train.tail()
+
+index = pd.date_range(start="2018-04-01", end="2018-10-31")
+forecast_train['Stime']=index
+forecast_train.set_index('Stime', inplace=True)
+forecast_train.head()
+X_train.head()
+forecast_train_error = X_train['Quantity']-forecast_train['Quantity_predicated']
+mean_forecast_train_error = np.mean(forecast_train_error)
+print(mean_forecast_train_error)
+
+mean_absolute_train_error = np.mean( np.abs(forecast_train_error) )
+print(mean_absolute_train_error)
+mean_squared_train_error = np.mean(forecast_train_error*forecast_train_error)
+print(mean_squared_test_error)
+
+
+rmse_train = np.sqrt(mean_squared_train_error)
+print(rmse_test)
+
+def mean_absolute_percentage_train_error(y_true, y_pred): 
+    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+MAPE= mean_absolute_percentage_train_error(X_train['Quantity'],forecast_train['Quantity_predicated'])
+MAPE
+
+pjme_train=pd.merge(X_train,forecast_train, left_index=True, right_index=True)
+MAPE= mean_absolute_percentage_train_error(pjme_train['Quantity'],pjme_train['Quantity_predicated'])
+MAPE
+pjme_train.plot(figsize=(15, 6))
+plt.show()
+
+all = pd.concat([pjme_train,pjme_test], sort=False)
+all.head()
+all.plot(figsize=(15, 6))
+plt.show()
+'''
+
+
+'''
+start_index = '2019-01-28'
+end_index = '2019-02-23'
+forecast = results.predict(start=start_index, end=end_index)
+
+forecast=forecast.to_frame(name=None)
+forecast=forecast.rename(columns = {0:'Quantity_predicated'})
+forecast.head()
+
+index = pd.date_range(start="2019-01-28", end="2019-02-23")
+forecast['Stime']=index
+forecast.set_index('Stime', inplace=True)
+
+forecast.head()
+
+all_l = pd.concat([pjme_train,pjme_test,forecast], sort=False)
+
+all_l.tail()
+
+all_l.plot(figsize=(15, 6))
+plt.show()
+'''
